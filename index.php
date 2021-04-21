296f215c133b4edb2846657fa50db8135f90b655
HarryNur <harry,nur.ft19@mail.umy.ac.id>
2021-04-21 13:49:00 +0700

    first commit

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-success bg-dark">
       <div class="container">
         <a class="navbar-brand" href="#">Data Mahasiswa</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,56 +26,54 @@
         </div>
       </div>
     </nav>
-    <div class="container data-mahasiswa text-right mt-5">
-        <table class=" table table-info table-striped">
-          <thead>
-            <tr>
-              <th scope="col">No.</th>
-              <th scope="col">Nama</th>
-              <th scope="col">NIM</th>
-              <th scope="col">Alamat</th>
-              <th scope="col">Aksi</th>
-            </tr>
-          </thead>
-        </table>
 
-        <tbody>
-            <?php
-            // memanggil config.php yang sudah kita buat
-            include 'config.php';
-            // membuat variabel untuk nomor mahasiswa yang akan diincrement
-            $no = 1;
-            //melakukan query
-            $mahasiswa = mysqli_query($koneksi, "select * from mahasiswa");
-            //looping data mahasiswa
-            while ($data = mysqli_fetch_array($mahasiswa)){
-                ?>
-                <!--menampilkan data mahasiswa ke dalam tabel-->
-                <table class=" table table-dark table-striped ">
+        <!--tabel-->
+    <div class = "container data-mahasiswa mt-3">
+        <table class=" table table-striped">
+            <thead class="table-info">
+                <tr class="table-active">
+                <th scope="col">No.</th>
+                <th scope="col">Nama</th>
+                <th scope="col">NIM</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // memanggil config.php yang sudah kita buat
+                include 'config.php';
+                // membuat variabel untuk nomor mahasiswa yang akan diincrement
+                $no = 1;
+                //melakukan query
+                $mahasiswa = mysqli_query($koneksi, "select * from mahasiswa");
+                //looping data mahasiswa
+                while($data = mysqli_fetch_array($mahasiswa)){
+                    ?>
+                    <!--menampilkan data mahasiswa ke dalam tabel-->
                     <tr>
-                        <!-- Increment nomor baris $no++ -->
-                        <td><?php echo $no++; ?></td>
-                        <!-- Menampilkan data -->
-                        <td><?php echo $data['nama'];?></td>
-                        <td><?php echo $data['nim'];?></td>
-                        <td><?php echo $data['alamat'];?></td>
-                        <!-- kolom ini untuk aksi data mahasiswa -->
-                        <td>
-                        <!-- Aksi edit dan delete di sini menggunakan btn-sm agar tomblnya kecil -->
-                        <!-- Link untuk masuk ke halaman edit -->
-                        <!-- edit.php?id=<?php echo $data['id']; ?> mengirimkan id dari data  mahasiswa ke halaman edit  -->
-                        <a href="edit.php?id=<?php echo $data['id']; ?>" class="btn-warning btn-sm text-white">Edit</a>
-                        <!-- Link untuk delete berdasarkan id, akan keluar confirm terlebih dahulu -->
-                        <a href="delete.php?id=<?php echo $data['id']; ?>" class="btn-danger btn-sm" onclick="return corfirm('Anda Yakin Akan Menghapus Data Mahasiswa Ini?')">Hapus</a>
-                        </td>
-                    </tr>    
-                 </table>
-
-            <?php
-            }
-            ?>
-        </tbody>
+                    <th scope="row"><?php echo $no++; ?></th>
+                    <!-- menampilkan data -->
+                    <td><?php echo $data['nama']; ?></td>
+                    <td><?php echo $data['nim']; ?></td>
+                    <td><?php echo $data['alamat']; ?></td>
+                    <!-- kolom ini untuk aksi data mahasiswa-->
+                    <td>
+                    <!-- Aksi edit dan delete di sini menggunakan btn-sm agar tomblnya kecil -->
+                    <!-- Link untuk masuk ke halaman edit -->
+                    <!-- mengirimkan id dari data  mahasiswa ke halaman edit  -->
+                    <a href="edit.php?id=<?php echo $data['id']; ?>" class="btn btn-warning btn-sm text-white">Edit</href=>
+                    <!-- Link untuk delete berdasarkan id, akan keluar confirm terlebih dahulu -->
+                    <a href="delete.php?id=<?php echo $data['id']; ?>" class="btn btn-danger btn-sm"onclick="return corfirm('Anda Yakin Akan Menghapus Data Mahasiswa Ini?')">Hapus</a>
+                    </td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
+
 
 
 
