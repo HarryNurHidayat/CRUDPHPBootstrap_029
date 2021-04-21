da5b8c83ae5b03457e992114f01eaf7866fbeddd
HarryNur <harry,nur.ft19@mail.umy.ac.id>
2021-04-21 12:33:21 +0700

    first commit

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
         </div>
       </div>
     </nav>
-    <div class="container data-mahasiswa text-right">
+    <div class="container data-mahasiswa text-right mt-5">
         <table class=" table table-info table-striped">
           <thead>
             <tr>
@@ -40,39 +40,41 @@
         </table>
 
         <tbody>
-          <?php
-          // memanggil config.php yang sudah kita buat
-          include 'config.php';
-          // membuat variabel untuk nomor mahasiswa yang akan diincrement
-          $no = 1;
-          //melakukan query
-          $mahasiswa = mysqli_query($koneksi, "select * from mahasiswa");
-          //looping data mahasiswa
-          while ($data = mysqli_fetch_array($mahasiswa)){
+            <?php
+            // memanggil config.php yang sudah kita buat
+            include 'config.php';
+            // membuat variabel untuk nomor mahasiswa yang akan diincrement
+            $no = 1;
+            //melakukan query
+            $mahasiswa = mysqli_query($koneksi, "select * from mahasiswa");
+            //looping data mahasiswa
+            while ($data = mysqli_fetch_array($mahasiswa)){
+                ?>
+                <!--menampilkan data mahasiswa ke dalam tabel-->
+                <table class=" table table-dark table-striped ">
+                    <tr>
+                        <!-- Increment nomor baris $no++ -->
+                        <td><?php echo $no++; ?></td>
+                        <!-- Menampilkan data -->
+                        <td><?php echo $data['nama'];?></td>
+                        <td><?php echo $data['nim'];?></td>
+                        <td><?php echo $data['alamat'];?></td>
+                        <!-- kolom ini untuk aksi data mahasiswa -->
+                        <td>
+                        <!-- Aksi edit dan delete di sini menggunakan btn-sm agar tomblnya kecil -->
+                        <!-- Link untuk masuk ke halaman edit -->
+                        <!-- edit.php?id=<?php echo $data['id']; ?> mengirimkan id dari data  mahasiswa ke halaman edit  -->
+                        <a href="edit.php?id=<?php echo $data['id']; ?>" class="btn-warning btn-sm text-white">Edit</a>
+                        <!-- Link untuk delete berdasarkan id, akan keluar confirm terlebih dahulu -->
+                        <a href="delete.php?id=<?php echo $data['id']; ?>" class="btn-danger btn-sm" onclick="return corfirm('Anda Yakin Akan Menghapus Data Mahasiswa Ini?')">Hapus</a>
+                        </td>
+                    </tr>    
+                 </table>
+
+            <?php
+            }
             ?>
-              <!--menampilkan data mahasiswa ke dalam tabel-->
-              <table class=" table table-dark table-striped ">
-                <!-- Increment nomor baris $no++ -->
-                <thead><?php echo $no++; ?></thead>
-                <!-- Menampilkan data -->
-                  <td><?php echo $data['nama'];?></td>
-                  <td><?php echo $data['nim'];?></td>
-                  <td><?php echo $data['alamat'];?></td>
-                <!-- kolom ini untuk aksi data mahasiswa -->
-                <td>
-                  <!-- Aksi edit dan delete di sini menggunakan btn-sm agar tomblnya kecil -->
-                  <!-- Link untuk masuk ke halaman edit -->
-                  <!-- edit.php?id=<?php echo $data['id']; ?> mengirimkan id dari data  mahasiswa ke halaman edit  -->
-                  <a href="edit.php?id=<?php echo $data['id']; ?>" class="btn-warning btn-sm text-white">Edit</a>
-                  <!-- Link untuk delete berdasarkan id, akan keluar confirm terlebih dahulu -->
-                  <a href="delete.php?id=<?php echo $data['id']; ?>" class="btn-danger btn-sm" onclick="return corfirm('Anda Yakin Akan Menghapus Data Mahasiswa Ini?')">Hapus</a>
-                </td>
-              </table>
-          <?php
-          }
-          ?>
         </tbody>
-      </table>
     </div>
 
 
